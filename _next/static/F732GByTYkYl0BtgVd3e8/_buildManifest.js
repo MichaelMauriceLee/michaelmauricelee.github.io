72f39008abccd6b93500741644c8ff1ebe7d28c2
MichaelMauriceLee <53194331+MichaelMauriceLee@users.noreply.github.com>
2020-09-27 23:00:27 +0000

    Deploying to gh-pages from  @ 9e705011bd5a8cba052381068acb492b5732a756 🚀

---
+++ _next/static/F732GByTYkYl0BtgVd3e8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/index":[a]}}("static\u002Fchunks\u002F78c5a7157caaf6ec6aab12c021fa75dc89145973.fbed823a99bbe548075f.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
